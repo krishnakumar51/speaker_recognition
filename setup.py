ae1df33ef9fc487b0c543e0cd7fc8a57f358a38f
Krishna kumar <godkrishnasskal@gmail.com>
2024-09-19 16:50:36 +0530

    comp

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="speaker-recognition-app",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "streamlit",
+        "sounddevice",
+        "numpy",
+        "matplotlib",
+        "torchaudio",
+        "speechbrain",
+        "scikit-learn",
+    ],
+    python_requires=">=3.10",
+    description="A Streamlit-based speaker recognition app",
+    author="Krishna Kumar",
+    author_email="godkrishna@gmail.com",
+    url="https://github.com/krishnakumar51/speaker_recognition.git",
+)
